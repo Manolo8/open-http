7f4b04f792b3aadee1a0aea0d27226a6b26319fd
Willian <willianmaysoares@gmail.com>
2022-03-23 22:52:24 -0300

    corrigido use-request e use-raw-datasource

--- src/hooks/use-raw-datasource.ts
+++ src/hooks/use-raw-datasource.ts
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
-import { Datasource } from '../other/datasource';
-import { DatasourceInput } from '../types/datasource-input';
-import { DatasourceProvider } from '../types/datasource-provider';
-import { Configurator, IConfigurator } from 'open-observable';
+import {useEffect, useState} from 'react';
+import {Datasource} from '../other/datasource';
+import {DatasourceInput} from '../types/datasource-input';
+import {DatasourceProvider} from '../types/datasource-provider';
+import {Configurator, IConfigurator} from 'open-observable';
 
 export const useRawDatasource = <TInput extends DatasourceInput<TOutput>, TOutput>(
     provider: DatasourceProvider<TInput, TOutput>,
     configure?: (configurator: IConfigurator<Datasource<TInput, TOutput>>) => void
 ): Datasource<TInput, TOutput> => {
-    const [{ datasource, configurator }] = useState(() => {
+    const [{datasource, configurator}] = useState(() => {
         const datasource = new Datasource(provider);
         const configurator = new Configurator(datasource);
-        return { datasource, configurator };
+        return {datasource, configurator};
     });
 
     useEffect(() => {
         configure?.(configurator);
 
-        datasource.refresh();
+        if (configurator.isCalled() || !configure) datasource.refresh();
 
         return () => configurator.reset();
     }, [configurator, configure, datasource]);
--- src/hooks/use-request.ts
+++ src/hooks/use-request.ts
@@ -1,22 +1,23 @@
-import { useEffect, useState } from 'react';
-import { RequestSource } from '../other/request-source';
-import { RequestSourceProvider } from '../types/request-source-provider';
-import { Configurator, IConfigurator } from 'open-observable';
+import {Configurator, IConfigurator} from 'open-observable';
+import {useEffect, useState} from 'react';
+
+import {RequestSource} from '../other/request-source';
+import {RequestSourceProvider} from '../types/request-source-provider';
 
 export const useRequest = <TInput, TOutput>(
     provider: RequestSourceProvider<TInput, TOutput>,
     configure?: (source: IConfigurator<RequestSource<TInput, TOutput>>) => void
 ): RequestSource<TInput, TOutput> => {
-    const [{ source, configurator }] = useState(() => {
+    const [{source, configurator}] = useState(() => {
         const source = new RequestSource<TInput, TOutput>(provider);
         const configurator = new Configurator(source);
-        return { source, configurator };
+        return {source, configurator};
     });
 
     useEffect(() => {
         configure?.(configurator);
 
-        source.refresh();
+        if (configurator.isCalled() || !configure) source.refresh();
 
         return () => configurator.reset();
     }, [configurator, configure, source]);
